d243344719a38671d95bf9a43f01a21590cf3a38
Алексей Леонтьев <leontev.leha06@bk.ru>
2024-12-19 10:28:02 +0500

    Добавили бд

---
+++ AnimeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimeTitles;
+
+public partial class AnimeType
+{
+    public short Id { get; set; }
+
+    public string AnimeOfType { get; set; } = null!;
+
+    public virtual ICollection<AnimeTitle> AnimeTitles { get; set; } = new List<AnimeTitle>();
+}
---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimeTitles;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateOnly DateOfRegistration { get; set; }
+
+    public virtual ICollection<AnimeTitle> AnimeTitles { get; set; } = new List<AnimeTitle>();
+}
